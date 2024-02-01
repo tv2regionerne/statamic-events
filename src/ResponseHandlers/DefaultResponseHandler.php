da9d9d1c4ca83af517ce837af0ff2e915cb5bb85
Ryan Mitchell <ryan@thoughtcollective.com>
2024-02-01 12:14:57 +0000

    Add default response handler and apply some tweaks to webhook

---
+++ src/ResponseHandlers/DefaultResponseHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tv2regionerne\StatamicEvents\ResponseHandlers;
+
+use Illuminate\Http\Response;
+use Statamic\Facades;
+
+class DefaultResponseHandler
+{
+    public handle(array $config, string $eventName, mixed $event, mixed $response = null)
+    {
+        if (! $response) {
+            return;
+        }
+
+        if ($response instanceof Response) {
+            if (! $response->ok()) {
+                return;
+            }
+
+            $response = $response->json();
+        }
+
+        if ($item = $event->asset) {
+            Facades\Asset::find($item->id())
+                ->data($response)
+                ->saveQuietly();
+
+            return;
+        }
+
+        if ($item = $event->entry) {
+            Facades\Entry::find($item->id())
+                ->data($response)
+                ->saveQuietly();
+
+            return;
+        }
+
+        if ($item = $event->term) {
+            Facades\Term::find($item->id())
+                ->data($response)
+                ->saveQuietly();
+
+            return;
+        }
+    }
+}
